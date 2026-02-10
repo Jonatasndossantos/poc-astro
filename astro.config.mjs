0bbe2246b8a6016062ed2095913946ba6b796fbc
jonatasndossantos <jonatas.nsantos2@senac.sp.edu.br>
2026-02-09 22:19:27 -0300

    chore: setup astro project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/en/": "/en/fullstack",
+  },
+  i18n: {
+    locales: ["en", "pt"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
+    }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "en",
+      locales: {
+        "en": "en",
+        "pt": "pt"
+      }
+    }
+  }), react()]
+});
